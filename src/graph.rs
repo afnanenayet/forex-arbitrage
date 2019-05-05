9e71c2d50740d716ee36fa57db1a7c820c8a05b7
Afnan Enayet <afnan@afnan.io>
2019-05-05 01:59:50 -0400

    Fix documentation
    Fix clippy lint

--- src/graph.rs
+++ src/graph.rs
@@ -3,7 +3,7 @@
 use std::collections::HashMap;
 
 /// A graph structure that contains a representation of edges and weights. The topmost keys
-/// represent vertices, and each vertex has a HashMap of associated weights.
+/// represent vertices, and each vertex has a `HashMap` of associated weights.
 pub type Graph<K, V> = HashMap<K, HashMap<K, V>>;
 
 /// Convenient type alias
